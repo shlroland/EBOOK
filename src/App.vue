bdb35d5a80b24681d79fa457307bb06530d12b4d
shlroland <shlroland1995@gmail.com>
2019-07-02 19:50:45 +0800

    环境搭建

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+<script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+export default {}
+  document.addEventListener('DOMContentLoaded',()=>{
+  const html = document.querySelector('html')
+  let fontSize=window.innerWidth/10
+  html.style.fontSize= fontSize + 'px'
+  
+})
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+
+<style lang="scss">
+
+
 </style>
+
